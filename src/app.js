5c4e753dc88f6798bdae75553ec8e4e1ee7319b1
SlmnFz <Salmanfz1681@gmail.com>
2024-02-26 13:39:38 +0330

    project starting point - app

---
+++ src/app.js
@@ -0,0 +1,32 @@
+// src/app.js
+require('dotenv').config(); // Load environment variables from .env file
+const express = require('express');
+const app = express();
+const http = require('http');
+const socketIo = require('socket.io');
+const port = process.env.PORT || 3000;
+const { client, connectToEventStore, disconnectFromEventStore } = require('../src/helpers/eventStore');
+
+const server = http.createServer(app)
+const io = socketIo(server)
+
+// Routes
+const usage = require('./routes/usage');
+
+app.use("/usage", usage)
+
+app.get('/', async (req, res) => {
+    await connectToEventStore();
+    res.send('Monitoring System is up and running!');
+});
+
+app.use((req, res) => {
+    console.log(`Requested Path: ${req.path} - ${req.method}`);
+    return res.status(404).json({
+        error: "❌ Not Found ❌"
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Monitoring System listening at http://localhost:${port}`);
+});
